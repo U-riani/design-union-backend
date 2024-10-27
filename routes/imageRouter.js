a2959d655aec5ca2deb32cde25cd96e7d9682e95
U-riani <sandropapiashvili@gmail.com>
2024-10-27 17:05:33 +0400

    f24

--- routes/imageRouter.js
+++ routes/imageRouter.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const imageController = require('../controllers/imageController');
-const upload = require('../middleware/imageMiddleware');
+// const upload = require('../middleware/imageMiddleware');
+const {handleImageUpload} = require('../middleware/imageMiddleware')
 
 // Upload route
 router.post('/upload', upload.single('image'), imageController.uploadImage);
