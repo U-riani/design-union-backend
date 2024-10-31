e73f681418f38ed406344b5bdf6fd888791a0bd4
U-riani <sandropapiashvili@gmail.com>
2024-11-01 00:36:29 +0400

    z5

--- controllers/visitController.js
+++ controllers/visitController.js
@@ -6,7 +6,7 @@ const bookVisit = async (req, res) => {
 
     const newVisit = new Visits({ name, email, phone, message, visitDate });
     await newVisit.save();
-    
+
     res.status(201).json({ message: "Visit successfully booked", newVisit });
   } catch (error) {
     res.status(400).json({ customError: "Error in booking visit", error });
@@ -15,10 +15,26 @@ const bookVisit = async (req, res) => {
 
 const getAvailableTime = async (req, res) => {
   try {
-    const visits = await Visits.find();
-    res.status(200).json(visits);
+    const { date } = req.query; // Expecting a query parameter 'date' in 'YYYY-MM-DD' format
+    const startOfDay = new Date(date);
+    startOfDay.setUTCHours(7, 0, 0, 0); // 11:00 AM in Georgia time (UTC+4)
+
+    const endOfDay = new Date(date);
+    endOfDay.setUTCHours(15, 0, 0, 0); // 7:00 PM in Georgia time (UTC+4)
+
+    // Find visits within the given date range
+    const visits = await Visits.find({
+      visitDate: {
+        $gte: startOfDay,
+        $lte: endOfDay,
+      },
+    });
+
+    res.status(200).json({ bookedVisits: visits });
   } catch (error) {
-    res.status(500).json({ customError: "Error in retrieving visit information", error });
+    res
+      .status(500)
+      .json({ customError: "Error in retrieving visit information", error });
   }
 };
 
--- models/Visit.js
+++ models/Visit.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const visitSchema = new mongoose.Schema({
-    name: {type: String},
-    email: {type: String},
-    phone: {type: String},
-    message: {type: String},
-    date: {type: Date, default: Date.now}
-})
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String },
+    message: { type: String },
+    visitDate: { type: Date, required: true }, // renamed to visitDate
+    createdAt: { type: Date, default: Date.now } // date the visit was booked
+});
 
-module.exports = mongoose.model("Visits", visitSchema)
+module.exports = mongoose.model("Visit", visitSchema);
