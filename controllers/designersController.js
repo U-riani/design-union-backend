d17d6588ef790a593af53392cad99b41b07e499d
U-riani <sandropapiashvili@gmail.com>
2024-11-07 15:45:18 +0400

    sddadasd

--- controllers/designersController.js
+++ controllers/designersController.js
@@ -46,7 +46,9 @@ const createDesigner = async (req, res) => {
       facebook: req.body.facebook,
       instagram: req.body.instagram,
       behance: req.body.behance,
-      images: req.fileUrls || [], // Use `fileUrls` from middleware
+      profilePhoto: req.fileUrls[0] || [],
+      projectPhoto: req.fileUrls[1] || [],
+      // images: req.fileUrls || [], // Use `fileUrls` from middleware
     };
 
     const newDesigners = new Designers(designerData);
