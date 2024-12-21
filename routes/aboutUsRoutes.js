ef11713a9681b364bb12c5d16ef7941a71c3ffe4
U-riani <sandropapiashvili@gmail.com>
2024-12-22 00:41:58 +0400

    add about us for main page

--- routes/aboutUsRoutes.js
+++ routes/aboutUsRoutes.js
@@ -1,16 +1,26 @@
-const express = require('express');
-const { getAboutUs, createAboutUs, updateAboutUs } = require('../controllers/aboutUsController');
-const { handleImageUpload } = require('../middleware/imageMiddleware');
+const express = require("express");
+const {
+  getAboutUs,
+  createAboutUs,
+  updateAboutUs,
+  createAboutUsMainPage,
+  getAboutUsMainPage,
+  updateAboutUsMainPage,
+} = require("../controllers/aboutUsController");
+const { handleImageUpload } = require("../middleware/imageMiddleware");
 
 const router = express.Router();
 
-router.get('/', getAboutUs);
+router.get("/", getAboutUs);
 
+router.post("/", handleImageUpload, createAboutUs);
 
-router.post('/', handleImageUpload,  createAboutUs);
+router.patch("/", handleImageUpload, updateAboutUs);
 
+router.get("/mainPage", getAboutUsMainPage);
 
-router.patch('/', handleImageUpload, updateAboutUs)
+router.post("/mainPage", handleImageUpload, createAboutUsMainPage);
 
+router.patch("/mainPage", handleImageUpload, updateAboutUsMainPage);
 
 module.exports = router;
